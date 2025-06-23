637d66bcc340ac03e8bc67789a878d748651eedc
mqmqmqzzz <blessingeveryday123@gmail.com>
2025-06-23 16:11:47 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello, ptt link</h1>
+      <div className="bg-blue-500 text-white p-4 rounded">
+      Tailwind is working!
+    </div>
+
+    </>
+  )
+
+
+}
+
+export default App
