623ebb849f54f2b631a871767dbdb71855447b69
mqmqmqzzz <blessingeveryday123@gmail.com>
2025-06-23 16:51:43 +0700

    fix: tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 
 import { useState } from 'react'
 import './App.css'
-
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello, ptt link</h1>
-      <div className="bg-blue-500 text-white p-4 rounded">
-      Tailwind is working!
-    </div>
+<div className="bg-blue-500 text-white p-4 rounded">
+  Hello Pttlink!
+</div>
+
 
     </>
   )
